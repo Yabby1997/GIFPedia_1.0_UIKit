ad9b5346a5494f2e9ba9af5ec835090fd30e7258
seunghun.yang <seunghun.yang@linecorp.com>
2023-06-05 22:12:20 +0900

    Initial Commit

---
+++ GIFPedia/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GIFPedia_UIKit
+//
+//  Created by USER on 2023/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
